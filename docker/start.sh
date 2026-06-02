061542ff1ce9c41d13c7147da356364a0a19ac79
sgami00 <sgami4256@gmail.com>
2026-06-02 09:55:02 +0800

    fix: move cache commands to runtime

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,14 @@ envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 php artisan config:cache
 php artisan view:cache
-
 php artisan migrate --force
+
+# Gumawa ng admin account (once lang, ignored kung nandoon na)
+php artisan tinker --execute="
+\App\Models\User::firstOrCreate(
+    ['email' => 'ateacher@school.edu'],
+    ['name' => 'Admin', 'password' => bcrypt('password123')]
+);
+"
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
