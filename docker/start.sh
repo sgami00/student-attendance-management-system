8d17f0414b4532752aa00002baf16e295afbcce9
sgami00 <sgami4256@gmail.com>
2026-06-02 09:44:54 +0800

    fix: move cache commands to runtime

--- docker/start.sh
+++ docker/start.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# I-substitute ang ${PORT} sa nginx config template
 envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-# Cache config at routes (dito na, may env variables na)
 php artisan config:cache
-php artisan route:cache
 php artisan view:cache
 
-# Run migrations
 php artisan migrate --force
 
-# I-start ang supervisor (nginx + php-fpm)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisor.conf
