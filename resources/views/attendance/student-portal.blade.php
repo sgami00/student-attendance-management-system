c44a6be5bf992a5b98483829c7dbd6674b84557d
yanquilingkim <yankimberyan@gmail.com>
2026-06-04 12:09:48 +0800

    Update student-portal.blade.php

--- resources/views/attendance/student-portal.blade.php
+++ resources/views/attendance/student-portal.blade.php
@@ -17,6 +17,7 @@
 <body class="px-4 py-8">
 
     {{--- Navbar ---}}
+    
     <nav class="max-w-3xl mx-auto flex items-center justify-between mb-8">
         <div class="flex items-center gap-2">
             <div class="bg-emerald-500 text-white rounded-xl p-2 shadow">
